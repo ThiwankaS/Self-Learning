74eb5d341b8681bb214b80df7dc2060df97b427d
Thiwanka Somachandra <thivankas@outlook.com>
2023-10-26 04:23:18 +0300

    Finalizing the routes to work with DB

--- Notes/Backend/index.js
+++ Notes/Backend/index.js
@@ -10,13 +10,6 @@ app.use(express.json());
 app.use(express.static('dist'));
 app.use(cors());
 
-const generateID = () => {
-    const maxId = notes.length > 0 
-    ? Math.max(...notes.map(n=>n.id))
-    : 0;
-    return maxId + 1;  
-}
-
 let notes = [
     {
         id : 1,
@@ -46,14 +39,9 @@ app.get('/api/notes',(request,response) => {
 })
 
 app.get('/api/notes/:id',(request,response) => {
-    const id = Number(request.params.id); 
-    const note = notes.find( note => note.id === id); 
-    
-    if(note){
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id).then((note)=>{
+        response.json(note); 
+    });
 })
 
 app.delete('/api/notes/:id',(request,response) => {
@@ -64,22 +52,21 @@ app.delete('/api/notes/:id',(request,response) => {
 
 app.post('/api/notes',(request,response) => {
     const body = request.body; 
-    if(!body.content){
+    if(body.content === undefined){
         return response.status(400).json({
-            error : "Content is empty"
+            error : "content missing"
         })
     }
 
-    const note = {
+    const note = new Note({
         content : body.content,
         important : body.important || false ,
-        id : generateID()
-    }
-    notes = notes.concat(note); 
-    response.json(note); 
-    console.log(note);
+    })
+    
+    note.save().then((savedNote)=>{
+        response.json(savedNote);
+    });
 })
-
 
 app.listen(PORT,() =>{
     console.log(`Server running on ${PORT}`); 
