8b8d8acf6c1accf14a4b2b10871a4b686f1e9030
Thiwanka Somachandra <thivankas@outlook.com>
2023-11-03 04:34:02 +0200

    Configure eslint and solve all the warning

--- Notes/Backend/index.js
+++ Notes/Backend/index.js
@@ -1,91 +1,31 @@
-require('dotenv').config();
-const express = require('express'); 
-const cors = require('cors');
-const Note = require('./models/note'); 
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
 
-const app = express();  
-const PORT = process.env.PORT;
+const app = express()
+const PORT = process.env.PORT
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message); 
-    if (error.name === 'CastError'){
-        return response.status(400).send({error : ' malformated id'});
-    }
-    next(error); 
-}
+const errorHandler = (error, request, response, next) => { console.error('....inside the middleware'); if (error.name === 'CastError'){ return response.status(400).send({ error : ' malformated id' }) } else if (error.name === 'ValidationError') { return response.status(400).send({ error : error.message }) } next(error) }
 
-app.use(errorHandler);
- 
+app.use(errorHandler)
 //Landing page
-app.get('/',(request,response,next) => {
-    response.send('<h1>Note web App</h1>')
-}); 
+app.get('/',(request,response,next) => { response.send('<h1>Note web App</h1>') } )
 //get all the notes
-app.get('/api/notes',(request,response, next) => {
-    Note.find({}).then((notes)=>{
-        response.json(notes);
-    }).catch(error => {
-        next(error)
-    })
-})
+app.get('/api/notes',(request,response, next) => { Note.find({}).then((notes) => { response.json(notes) }).catch(error => { next(error) }) })
 //get one note by Id
-app.get('/api/notes/:id',(request,response,next) => {
-    Note.findById(request.params.id).then((note)=>{
-        if(note){
-            response.json(note); 
-        } else {
-            response.status(404).send({error : ' object not found '}); 
-        }
-    }).catch(error => {
-        next(error)
-    })
-})
+app.get('/api/notes/:id',(request,response,next) => { Note.findById(request.params.id).then((note) => { if(note){ response.json(note) } else { response.status(404).send({ error : ' object not found ' }) }}).catch(error => { next(error)}) })
 //delete a note
-app.delete('/api/notes/:id',(request,response, next) => {
-    Note.findByIdAndDelete(request.params.id).then(result => {
-        response.status(204).end(); 
-    }).catch(error => {
-        next(error)
-    })
-})
+app.delete('/api/notes/:id',(request,response, next) => { Note.findByIdAndDelete(request.params.id).then(result => { response.status(204).end() }).catch(error => { next(error) })})
 //add new note
-app.post('/api/notes',(request,response,next) => {
-    const body = request.body; 
-    if(body.content === undefined){
-        return response.status(400).json({
-            error : "content missing"
-        })
-    }
-
-    const note = new Note({
-        content : body.content,
-        important : body.important || false ,
-    })
-    
-    note.save().then((savedNote)=>{
-        response.json(savedNote);
-    }).catch(error => {
-        next(error)
-    })
-})
+app.post('/api/notes',(request,response,next) => { const body = request.body; const note = new Note({ content : body.content, important : body.important || false } ); note.save().then((savedNote) => { response.json(savedNote) }).catch(error => { console.log('Calling middleware'); next(error) }) })
 //update a record
-app.put('/api/notes/:id',(request, response, next) => {
-    const body = request.body; 
-    const note = {
-            content : body.content,
-            important : body.important,
-    }
-    Note.findByIdAndUpdate(request.params.id, note, { new : true }).then( updatesRecord => {
-        response.json(updatesRecord)
-    }).catch( error => {
-        next(error)
-    })
-})
+app.put('/api/notes/:id',(request, response, next) => { const { content , important } = request.body; Note.findByIdAndUpdate(request.params.id, { content , important } , { new : true , runValidators : true, context : 'query'  }).then( updatesRecord => { response.json(updatesRecord) }).catch( error => { next(error)})})
 
-app.listen(PORT,() =>{
-    console.log(`Server running on ${PORT}`); 
-}); 
+app.use(errorHandler)
+
+app.listen(PORT,() => { console.log(`Server running on ${PORT}`) })
--- Notes/Backend/models/note.js
+++ Notes/Backend/models/note.js
@@ -1,27 +1,17 @@
-require('dotenv').config();
-const mongoose = require('mongoose'); 
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URL; 
+const url = process.env.MONGODB_URL
 
-mongoose.set('strictQuery',false); 
-console.log(`connecting to DB.....`); 
-mongoose.connect(url).then((result)=>{
-    console.log(`connection sucessful!`); 
-}).catch((error)=>{
-    console.log(`connection error`,error.message);
-});
-
-const noteSchema = new mongoose.Schema({
-    content : String,
-    important : Boolean,
+mongoose.set('strictQuery',false)
+console.log('connecting to DB.....')
+mongoose.connect(url).then((result) => { console.log('connection sucessful!')
+}).catch((error) => { console.log('connection error',error.message)
 })
 
-noteSchema.set('toJSON',{
-    transform : (document,returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id; 
-        delete returnedObject.__v; 
-    }
+const noteSchema = new mongoose.Schema({ content : { type : String, minLength : 5, required : true }, important : Boolean })
+
+noteSchema.set('toJSON',{ transform : (document,returnedObject) => { returnedObject.id = returnedObject._id.toString(); delete returnedObject._id; delete returnedObject.__v }
 })
 
-module.exports = mongoose.model('Note',noteSchema);
+module.exports = mongoose.model('Note',noteSchema)
