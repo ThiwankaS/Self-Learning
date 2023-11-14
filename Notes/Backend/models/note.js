a92fe67e700e8a4272524646d4ae6f3425e2421d
Thiwanka Somachandra <thivankas@outlook.com>
2023-11-14 04:26:11 +0200

    supertest

--- Notes/Backend/models/note.js
+++ Notes/Backend/models/note.js
@@ -9,13 +9,19 @@ const url = config.MONGODB_URL
 
 mongoose.set('strictQuery',false)
 logger.info('connecting to DB.....')
-mongoose.connect(url).then((result) => { logger.info('connection sucessful!')
-}).catch((error) => { logger.info('connection error',error.message)
+
+mongoose.connect(url).then((result) => {
+  logger.info('connection sucessful!')
+}).catch((error) => {
+  logger.info('connection error',error.message)
 })
 
 const noteSchema = new mongoose.Schema({ content : { type : String, minLength : 5, required : true }, important : Boolean })
 
-noteSchema.set('toJSON',{ transform : (document,returnedObject) => { returnedObject.id = returnedObject._id.toString(); delete returnedObject._id; delete returnedObject.__v }
+noteSchema.set('toJSON',{ transform : (document,returnedObject) => {
+  returnedObject.id = returnedObject._id.toString()
+  delete returnedObject._id; delete returnedObject.__v
+}
 })
 
 module.exports = mongoose.model('Note',noteSchema)
