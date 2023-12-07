21e554f7b8252d92ded71dd737b937922a293c65
Thiwanka Somachandra <thivankas@outlook.com>
2023-12-07 05:38:29 +0200

    Rendering the component for tests

--- Notes/FrontEnd/src/components/Note.jsx
+++ Notes/FrontEnd/src/components/Note.jsx
@@ -1,18 +1,14 @@
-import PropTypes from 'prop-types'
-
 const Note = ({ note,toggleImportance }) => {
-  const label = note.important ? 'make not important' : 'make important'
+  const label = note.important
+    ? 'make not important'
+    : 'make important'
 
   return (
-    <li className="note">{ note.content }
+    <li className='note'>
+      { note.content }
       <button onClick={toggleImportance}>{ label }</button>
     </li>
   )
-}
-
-Note.propTypes = {
-  note : PropTypes.object.isRequired,
-  toggleImportance : PropTypes.func.isRequired
 }
 
 export default Note
