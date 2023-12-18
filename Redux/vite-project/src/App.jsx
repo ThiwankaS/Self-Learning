eb6c17a950c772bc222329c9e9fb2418ec41d398
Thiwanka Somachandra <thivankas@outlook.com>
2023-12-18 03:58:34 +0200

    Redux - notes

--- Redux/vite-project/src/App.jsx
+++ Redux/vite-project/src/App.jsx
@@ -1,28 +1,43 @@
 import { createStore } from 'redux'
 
-function App() {
- 
-  const counterReducer = (state = 0, action) => {
-    switch(action.type){
-      case 'INCREMNT'  : return state + 1 
-      case 'DECREMENT' : return state - 1
-      case 'ZERO'      : return 0
-      default          : return state 
-    }
+const noteReducer = (state = [], action) => {
+  if(action.type === 'NEW_NOTE'){
+    state.push(action.payload)
+    return state
   }
-  const store = createStore(counterReducer)
-  store.subscribe(() => {
-    const storeNow = store.getState()
-    console.log(storeNow)
-  })
-  store.dispatch({ type : 'INCREMNT' })
-  store.dispatch({ type : 'INCREMNT' })
-  store.dispatch({ type : 'INCREMNT' })
-  store.dispatch({ type : 'ZERO' })
-  store.dispatch({ type : 'DECREMENT' })
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type : 'NEW_NOTE',
+  payload : {
+    content : 'the app state is in redux store',
+    important : true,
+    id : 1
+  }
+})
+
+store.dispatch({
+  type : 'NEW_NOTE',
+  payload : {
+    content : 'state changes are made with actions',
+    important : false,
+    id : 2
+  }
+})
+
+
+const App = () => {
+ 
   return (
     <div>
-      <h4>Count {store.getState()} </h4>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>{note.content }<strong>{ note.important ? 'important' : ''}</strong></li>
+        )}
+      </ul>
     </div>
   )
 }
--- Redux/vite-project/src/main.jsx
+++ Redux/vite-project/src/main.jsx
@@ -1,43 +1,6 @@
-import { createStore } from 'redux'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const counterReducer = (state = 0, action) => {
-  switch(action.type){
-    case 'INCREMENT' : return state + 1
-    case 'DECREMENT' : return state - 1
-    case 'ZERO' : return 0
-    default : return state
-  }
-}
-
-const store = createStore(counterReducer)
-
-const App = () => {
-  return (
-    <div>
-      <div>
-        {store.getState()}
-      </div>
-      <div>
-        <button onClick={() => store.dispatch({ type : 'INCREMENT'})}>plus </button>
-      </div>
-      <div>
-        <button onClick={() => store.dispatch({ type : 'DECREMENT'})}>minus</button>
-      </div>
-      <div>
-        <button onClick={() => store.dispatch({ type : 'ZERO'})}>zero</button>
-      </div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 
