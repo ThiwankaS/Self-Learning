8dba520980898647fa6f470a6374f95428fb31ae
Thiwanka Somachandra <thivankas@outlook.com>
2023-12-20 19:01:28 +0200

    Uncontrolled form

---
+++ Redux/note-redux/src/main.jsx
@@ -0,0 +1,57 @@
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+const generateID = () => (Math.random()*1000000).toFixed(0)
+const App = () => {
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value =''
+    store.dispatch({
+        type : 'NEW_NOTE',
+        payload : {
+            content,
+            important : true,
+            id : generateID()
+        }
+    })
+  }
+  const toggleImportance = (id) => {
+    store.dispatch({
+        type : 'TOGGLE_IMPORTANCE',
+        payload : { id }
+    })
+  }
+  return (
+    <div>
+      <div>
+        <form onSubmit={addNote}>
+            <input name='note' />
+            <button type='submit'>add</button>
+        </form>
+      </div>
+      <div>
+        <ul>
+            {store.getState().map(note => <li 
+                                            key={note.id} 
+                                            onClick={() => toggleImportance(note.id)}
+                                          >
+                                            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+                                         </li>)}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+    root.render(<App />)
+}
+renderApp()
+store.subscribe(renderApp)
---
+++ Redux/note-redux/src/reducers/noteReducer.test.js
@@ -0,0 +1,46 @@
+import noteReducer from './noteReducer'
+import deepFreez from 'deep-freeze'
+
+describe('noteReducer' ,() => {
+    test('returns new state with action NEW_NOTE', () => {
+        const state = []
+        const action = {
+            type : 'NEW_NOTE',
+            payload : {
+                content : 'the app state is in redux store',
+                important : true,
+                id : 1
+            }
+        }
+        deepFreez(state)
+        const newState = noteReducer(state,action)
+        expect(newState).toHaveLength(1)
+        expect(newState).toContainEqual(action.payload)
+
+    })
+    test('returns new state with action TOGGLE_IMPORTANCE', () => {
+        const state = [
+            {
+                content : 'the app state is in redux store',
+                important : true,
+                id : 1
+            },{
+                content : 'state changes are made with actions',
+                important : false,
+                id : 2
+            }
+        ]
+        const action = {
+            type : 'TOGGLE_IMPORTANCE',
+            payload : {
+                id : 2
+            }
+        }
+        deepFreez(state)
+        const newState = noteReducer(state,action)
+        expect(newState).toHaveLength(2)
+        expect(newState).toContainEqual(state[0])
+        const updatedObj = {...state[1],important : true }
+        expect(newState).toContainEqual(updatedObj)
+    })
+})
