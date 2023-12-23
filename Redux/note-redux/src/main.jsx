67eafed8e62adfdc63e7afaab5dc3d8dcfd7ff45
Thiwanka Somachandra <thivankas@outlook.com>
2023-12-23 04:01:05 +0200

    Refactoring the code with Redux toolkit

--- Redux/note-redux/src/main.jsx
+++ Redux/note-redux/src/main.jsx
@@ -1,20 +1,20 @@
 import ReactDOM from 'react-dom/client'
-import { createStore,combineReducers } from 'redux'
-import noteReducer, { createNote } from './reducers/noteReducer'
-import filterReducer, { filterChange } from './reducers/filterReducer'
+import { configureStore } from '@reduxjs/toolkit'
+
 import { Provider } from 'react-redux'
 import App from './App'
 
-const reducer = combineReducers({
-    notes : noteReducer,
-    filter : filterReducer
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({
+    reducer : {
+        notes : noteReducer,
+        filter : filterReducer
+    }
 })
 
-const store = createStore(reducer)
-
-store.subscribe(()=> console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
