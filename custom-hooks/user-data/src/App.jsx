dd9105080e36bdc56b73ef73602548848b1121d4
Thiwanka Somachandra <thivankas@outlook.com>
2024-01-08 04:06:01 +0200

    Simple user data app

--- custom-hooks/user-data/src/App.jsx
+++ custom-hooks/user-data/src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [name,setName] = useState('')
+  const [born,setBorn] = useState('')
+  const [height,setHeight] = useState('')
 
   return (
-    <>
+    <div>
+      <form>
+        <div>name : <input type='text'  value={name} onChange={(event) => {setName(event.target.value)}}/></div>
+        <div>birthdate : <input type='text'  value={born} onChange={(event) => {setBorn(event.target.value)}}/></div>
+        <div>height : <input type='text'  value={height} onChange={(event) => {setHeight(event.target.value)}}/></div>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {name} | {born} | {height}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
