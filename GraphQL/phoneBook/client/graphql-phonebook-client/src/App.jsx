305f98351d7af8ae70dcc69759311477620a5e22
Thiwanka Somachandra <thivankas@outlook.com>
2024-02-09 04:51:17 +0200

    Add client folder after implement Friend List

---
+++ GraphQL/phoneBook/client/graphql-phonebook-client/src/App.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable react/prop-types */
+import { useQuery } from '@apollo/client'
+import { ALL_PERSONS } from './assets/queries'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
+import { useState } from 'react'  
+
+
+
+const App = () => {
+
+  const result = useQuery(ALL_PERSONS)
+  const [ errorMessage,setErrorMessage ] = useState(null)
+
+  if(result.loading){
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(()=>{
+      setErrorMessage(null)
+    },
+    5000)
+  }
+
+  return (
+  <div>
+    <Notify errorMessage={errorMessage}/>
+    <Persons persons={result.data.allPersons} />
+    <PersonForm setError={notify}/> 
+    <PhoneForm setError={notify}/>
+  </div>
+)}
+
+export default App
+
+const Notify = ({ errorMessage }) => {
+  if(!errorMessage){
+    return null
+  }
+  return (
+    <div style={{color : 'red'}}>{errorMessage}</div>
+  )
+}
