cc4c9be278523e575f72cdd69ab8143ecdcdfa70
Thiwanka Somachandra <thivankas@outlook.com>
2024-03-17 07:46:55 +0200

    The horrors of any and eslint implementaion

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import { calculator } from './calculator';
+
 const app = express();
 
 app.get('/ping',(_req,res) => {
     res.send('pong');
-})
+});
+
+app.post('/calculate',( req,res ) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { value1, value2, op } = req.query;
+    if( !value1 || isNaN(Number(value1))){
+        return res.status(400).send({ error : ' argument(s) malformatted '})
+    }
+    if( !value2 || isNaN(Number(value2))){
+        return res.status(400).send({ error : ' argument(s) malformatted '})
+    }
+    if( !op || !(op === 'multiply' || 'add' || 'divide')){
+        return res.status(400).send({ error : ' argument(s) malformatted '})
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator( Number(value1), Number(value2), op );
+    return res.send({ result });
+});
 
 const PORT = 3003; 
 
 app.listen(PORT,() => {
-    console.log(`Serever running on the port ${PORT}`)
+    console.log(`Serever running on the port ${PORT}`);
 });
--- typescript/multiply.ts
+++ typescript/multiply.ts
@@ -5,7 +5,7 @@ interface MultipyValues {
 
 const multiplicator = (a : number, b : number, printText : string) => {
     console.log(printText, a * b);
-}
+};
 
 const parseArguments = (args : string[]) : MultipyValues => {
     if(args.length < 4) throw new Error('Not enough arguments');
@@ -14,18 +14,17 @@ const parseArguments = (args : string[]) : MultipyValues => {
         return {
             value1 : Number(args[2]),
             value2 : Number(args[3])
-        }
+        };
     } else {
-        throw new Error('Provided values are not numbers!')
+        throw new Error('Provided values are not numbers!');
     } 
-}
-
+};
 
 try {
     const { value1, value2 } = parseArguments(process.argv);
-    multiplicator(value1,value2,`Multiplied ${value1} and ${value2}, the result is :`)
+    multiplicator(value1,value2,`Multiplied ${value1} and ${value2}, the result is :`);
 } catch ( error : unknown){
-    let errorMessage : string = 'Something bad happened.'
+    let errorMessage : string = 'Something bad happened.';
     if(error instanceof Error){
         errorMessage += ' Error : '+ error.message;
     }
